a078c7f9345ba815ae875b999153fd676980e12a
GPU-Fried-Eggs <seele.peng@gmail.com>
2024-03-18 02:03:32 +0200

    feat: support emitting source map for `Joiner` #10

--- src/magic_string/source_map.rs
+++ src/magic_string/source_map.rs
@@ -62,6 +62,52 @@ impl<'s> MagicString<'s> {
         }
     }
 
+    pub fn source_map_to_mapping(&self, mut mappings: &mut Mappings) -> Vec<String> {
+        let locator = Locator::new(&self.source);
+
+        self.intro.iter().for_each(|frag| {
+            mappings.advance(frag);
+        });
+
+        let mut names = vec![];
+
+        self.iter_chunks().for_each(|chunk| {
+            chunk.intro.iter().for_each(|frag| {
+                mappings.advance(frag);
+            });
+
+            let original_content = chunk.span.text(&self.source);
+
+            if self.filename.is_some() {
+                let name_idx = if chunk.keep_in_mappings && chunk.is_edited() {
+                    let idx = names
+                        .iter()
+                        .enumerate()
+                        .find_map(|(idx, name)| (name == original_content).then_some(idx))
+                        .unwrap_or_else(|| {
+                            let next_idx = names.len();
+                            names.push(original_content.to_string());
+                            next_idx
+                        });
+                    debug_assert!(idx < names.len());
+                    Some(idx as u32)
+                } else {
+                    None
+                };
+
+                mappings.add_chunk(chunk, &locator, 0, &self.source, name_idx);
+            } else {
+                mappings.advance(original_content);
+            }
+
+            chunk.outro.iter().for_each(|frag| {
+                mappings.advance(frag);
+            });
+        });
+
+        names
+    }
+
     pub fn source_map(&self, opts: SourceMapOptions) -> SourceMap {
         let decoded_map = self.generate_decoded_source_map(opts);
         decoded_map.into_source_map()
